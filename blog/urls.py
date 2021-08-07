6f82895b7bf8aa1051e7f3828d2b22ca5957141e
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-08-07 17:24:28 -0300

    mod blog/url #2

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     #path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('create', views.PostCreate.as_view(), name='post_create'),
     path('sobre', views.sobre, name='sobre'),
 ]
