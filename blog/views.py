802216939ec9b18a326a9d70a594870a72978ded
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-08-14 17:25:02 -0300

    mod views #5

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,14 @@ from django.views import generic
 from .models import Post
 from .forms import CommentForm
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 2 #paginação
+
 #class PostDetail(generic.DetailView):
 #    model = Post
 #    template_name = 'post_detail.html'
